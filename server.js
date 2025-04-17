89c0117593d683a5d7843849b83d30a0dcdf3e19
sakshi pimpale <sakshipimpale88@gmail.com>
2025-04-17 11:52:22 +0530

    chnages after adding api commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors({
   origin: [
     'http://localhost:5500',
     'http://127.0.0.1:5501',
-    'https://prahas-frontend.vercel.app' // Your frontend domain
+    'https://prasa-main.vercel.app' // Your frontend domain
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
