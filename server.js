c7b173047189b4e272d9364615fbe41edad6649c
sakshi pimpale <sakshipimpale88@gmail.com>
2025-04-18 11:44:55 +0530

    update in server.js 2

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ const pool = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: 12464,
+  port: 25644,
   ssl: {
     rejectUnauthorized: false,
   },
